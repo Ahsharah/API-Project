26c969d28381c89d14f04cd650e3869c015853b7
Alexandria <walkerciara24@gmail.com>
2024-10-22 15:23:54 -0400

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -9,3 +9,23 @@ export function createPokemonCard(pokemon) {
     const card = document.createElement('div');
     card.className = 'pokemon-card';
     card.dataset.id = pokemon.id;
+     // Capitalize the first letter of the name
+     const displayName = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+    
+     card.innerHTML = `
+         <img 
+             src="${pokemon.sprites.front}" 
+             alt="${displayName}"
+             loading="lazy"
+         >
+         <h3>${displayName}</h3>
+         <div class="pokemon-types">
+             ${pokemon.types.map(type => 
+                 `<span class="type-badge ${type}">${type}</span>`
+             ).join('')}
+         </div>
+     `;
+     
+     return card;
+ }
+ 
