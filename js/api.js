d92b82138b1ea7615e7be65d4563e50760c028d5
Alexandria <walkerciara24@gmail.com>
2024-10-22 14:57:05 -0400

    Update api.js

---
+++ js/api.js
@@ -0,0 +1,13 @@
+// js/api.js
+
+/**
+ * Base URL for the PokeAPI
+ * Using the v2 endpoint which is the most stable version I can find.
+ */
+const BASE_URL = 'https://pokeapi.co/api/v2';
+
+/**
+ * Default limit
+ * PokeAPI allows up to 100 items per request
+ */
+const DEFAULT_LIMIT = 20;
