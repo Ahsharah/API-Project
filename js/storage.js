85a3df9a8c354390a85b5b9aa00a344e70147a0c
Alexandria <walkerciara24@gmail.com>
2024-10-24 01:18:37 -0400

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -14,4 +14,16 @@ export function getFavorites() {
         console.error('Error getting favorites:', error);
         return new Set();
     }
+}/**
+ * Get set of Pokemon IDs in compare list from local storage
+ * @returns {Set<string>} Set of Pokemon IDs in compare list
+ */
+export function getCompareList() {
+    try {
+        const compareList = JSON.parse(localStorage.getItem('pokemonCompareList')) || [];
+        return new Set(compareList);
+    } catch (error) {
+        console.error('Error getting compare list:', error);
+        return new Set();
+    }
 }
